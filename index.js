0c832845b1d77d6b21cdec8332c88b1d77254f46
Jin Young Park <jy2045@sogang.ac.kr>
2021-02-24 01:28:38 +0900

    Query 구현 실습

---
+++ index.js
@@ -0,0 +1,50 @@
+const database = require("./database");
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Query {
+    teams: [Team]
+    equipments: [Equipment]
+    supplies: [Supply]
+    team(id: Int): Team
+  }
+  type Team {
+    id: Int
+    manager: String
+    office: String
+    extension_number: String
+    mascot: String
+    cleaning_duty: String
+    project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: Int
+  }
+`;
+
+const resolvers = {
+  Query: {
+    teams: () =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter((supply) => supply.team === team.id);
+        return team;
+      }),
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
+    team: (parent, args, context, info) => database.teams.filter((team) => team.id === args.id)[0],
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
